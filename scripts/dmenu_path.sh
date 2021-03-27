fd7a8c88ee225672e43fae6495b6bd2e5c019ba3
Houjun Liu <hliu@shabang.cf>
2021-03-27 16:48:27 -0700

    scripts

---
+++ scripts/dmenu_path.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Import the colors
+. "${HOME}/.cache/wal/colors.sh"
+
+dmenu_run -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn 'Hack Nerd Font:pixelsize=14:antialias=true:autohint=true'
+
