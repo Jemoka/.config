2ff293dd09afd722f4ec78c0011ecdfc4587a7a4
Houjun Liu <hliu@shabang.cf>
2021-04-04 22:15:25 -0700

    nice

--- scripts/dmenu_toggl.sh
+++ scripts/dmenu_toggl.sh
@@ -7,15 +7,22 @@ TIMERS=`python -m toggl -s ls -f description | sort -r | uniq`
 
 CHOSEN=$(echo "${TIMERS}\n\$tstart\n\$tstop" | dmenu -p "Timers" -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn 'Hack Nerd Font:pixelsize=14:antialias=true:autohint=true' -i)
 
-case $CHOSEN in
-    "\$tstop")
-        python -m toggl stop
-        ;;
-    "\$tstart")
-        python -m toggl start
-        ;;
-    *)
-        python -m toggl continue $CHOSEN
-        ;;
-esac
+[[ $TIMERS =~ (^|[[:space:]])$CHOSEN($|[[:space:]]) ]] && true || false
+
+if [ $? -eq 0 ]; then
+    case $CHOSEN in
+        "\$tstop")
+            python -m toggl stop
+            ;;
+        "\$tstart")
+            python -m toggl start
+            ;;
+        *)
+            python -m toggl continue $CHOSEN
+            ;;
+    esac
+else
+    python -m toggl start $CHOSEN
+fi
+
 
