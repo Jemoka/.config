0ea17079974894cebd4241706401e92f41424302
Houjun Liu <hliu@shabang.cf>
2021-07-25 10:13:06 -0700

    more!

--- scripts/dmenu_path.sh
+++ scripts/dmenu_path.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-dmenu_run -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn 'Hack Nerd Font:pixelsize=17:antialias=true:autohint=true'
+PATH="/opt/resolve/bin/:${PATH}" dmenu_run -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn 'Hack Nerd Font:pixelsize=17:antialias=true:autohint=true'
 
