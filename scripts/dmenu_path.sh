c045fd1bd41018bbb3e05304e8a320b6909ab532
Houjun Liu <hliu@shabang.cf>
2021-08-01 22:30:58 -0700

    empty

--- scripts/dmenu_path.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Import the colors
-. "${HOME}/.cache/wal/colors.sh"
-
-dmenu_run -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn 'Hack Nerd Font:pixelsize=22:antialias=true:autohint=true'
-
--- scripts/stupid_xrandr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-xrandr --output HDMI-1 --mode 1280x1024 --left-of eDP-1
-xrandr --output HDMI-1 --mode 1600x900 --left-of eDP-1
-
-
-
-
